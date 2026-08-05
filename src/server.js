8d960afd5be2953973e9e041eb5e0561954b9811
Hugo <hugo.belarmino10@gmail.com>
2026-08-05 16:19:44 -0300

    Cria servidor Express

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.json({
+    mensagem: "API Elektro funcionando!"
+  });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
